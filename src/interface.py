69c222aa7c1bf52e7434180887ff39588f969ad6
Btzel <buraktsoftware@gmail.com>
2025-01-12 16:28:48 +0300

    cont panel

--- src/interface.py
+++ src/interface.py
@@ -1,3 +1,4 @@
+import sys
 from PyQt5 import QtWidgets, uic
 from PyQt5.QtWidgets import QPushButton, QStackedWidget
 import os
@@ -8,9 +9,18 @@ class Ui_MainWindow(QtWidgets.QMainWindow):
         super(Ui_MainWindow, self).__init__()
         ui_path = os.path.join(os.path.dirname(__file__), 'interface.ui')
         uic.loadUi(ui_path, self)  # Load the UI file
-        self.button_to_panel_bind()
+        #panel
+        self.currentPanel = 0
+        self.handle_button_clicks()
         
-    def button_to_panel_bind(self):
-        self.pushButton_6.clicked.connect(lambda: self.stackedWidget_2.setCurrentIndex(0))
-        self.pushButton_8.clicked.connect(lambda: self.stackedWidget_2.setCurrentIndex(1))
+    def handle_button_clicks(self):
+        self.toolButton.clicked.connect(lambda: self.stackedWidget_2.setCurrentIndex(0))
+        self.toolButton_3.clicked.connect(lambda: self.stackedWidget_2.setCurrentIndex(1))
         
+        
+    
+
+        
+
+    
+
